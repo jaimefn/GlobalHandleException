4b848e209684e17d7f86f9dba657657a148d9dc6
jaime <jaime@jaime.nascimento>
2019-11-27 23:21:56 -0300

    projeto maven criado

---
+++ src/test/java/com/example/HandleGlobalException/HandleGlobalExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HandleGlobalException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandleGlobalExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
